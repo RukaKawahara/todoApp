8d675a248df82ec3eed426cba41c6ddab6da8cc1
河原　瑠花 <kawhara_ruka@waku-2.com>
2024-06-13 18:31:54 +0900

    psqlの書き方に修正しました

--- index.js
+++ index.js
@@ -13,69 +13,60 @@ app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.engine('ejs', require('ejs').__express);
 
-const pool = new pg.Pool({
+const config = {
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
   port: process.env.PORT,
+  ssl: true,
+};
 
-  // Render.comのDBではSSLが求められる
-  ssl: {
-    rejectUnauthorized: false, // 証明書の検証はいったん無しで
-  },
-  max: 10,
-})
+var pool = new pg.Pool(config);
+pool.connect();
 
-pool.on('error', (err) => {
-  console.error('Unexpected error on idle clients', err);
-  process.exit(-1); // アプリケーションの異常終了
+app.get('/', async(req, res) => {
+  const insertQuery = 'SELECT * FROM todo ORDER BY id';
+  try {
+    const result = await pool.query(insertQuery);
+    res.render('index', {todos:result.rows});
+  } catch(err) {
+    console.error(err.stack)
+  }
 });
 
-app.get('/', (req, res) => {
-  pool.query('SELECT * FROM todo',(err, results) => {
-      if(err){
-        console.log('psqlエラー(select):' + err.stack);
-        return;
-      }
-      res.render('index.ejs',{todos:results});
-    }
-  );
-});
-
-app.post('/add', (req, res) => {
+app.post('/add', async(req, res) => {
   const text = req.body.text;
-  const insertQuery = 'INSERT INTO todo (text, checked) VALUES (?, ?)';
-  pool.query(insertQuery, [text, false], (err, results) => {
-    if (err) {
-      console.error('psqlエラー(insert):', err);
-      return res.status(500).json({ error: 'タスクの追加に失敗しました。' });
-    }
+  //INSERTクエリを作成する。※Value値は必ず「$1,$2,$3,...」とすること！(ほかの変数にすると構文エラーになる
+  const insertQuery = 'INSERT INTO todo (text, checked) VALUES ($1, $2) RETURNING *';
+  try {
+    await pool.query(insertQuery, [text, false]);
     res.redirect('/');
-  });
+  } catch(err) {
+    console.error(err.stack)
+  }
 });
 
-app.post('/delete/:id',(req,res)=>{
-	const sql = "DELETE FROM todo WHERE id = ?";
-	pool.query(sql,[req.params.id],(err,result,fields) => {
-		if (err) {
-      console.error('psqlエラー(delete):', err);
-      return res.status(500).json({ error: 'タスクの削除に失敗しました。' });
-    }
-		res.redirect('/');
-	})
+
+app.post('/delete/:id', async(req, res) => {
+  const sql = "DELETE FROM todo WHERE id = $1 RETURNING *";
+  try {
+    await pool.query(sql,[req.params.id]);
+    res.redirect('/');
+  } catch(err) {
+    console.error(err.stack)
+  }
 });
 
-//更新アクション
-app.post('/update/:id', (req, res) => {
-  const sql = "UPDATE todo SET checked=? WHERE id = ?";
-  pool.query(sql,[req.body.checked, req.params.id],(err,results)=>{
-    if (err) {
-      console.error('psqlエラー(update):', err);
-      return res.status(500).json({ error: 'タスクの更新に失敗しました。' });
-    }
-		res.redirect('/');
-  })
+// checkboxの更新
+app.post('/update/:id', async(req, res) => {
+  const sql = "UPDATE todo SET checked=$1 WHERE id = $2 RETURNING *";
+  try {
+    await pool.query(sql,[req.body.checked, req.params.id]);
+    res.redirect('/');
+  } catch(err) {
+    console.error(err.stack)
+  }
 });
 
 // ローカルサーバーを起動
--- public/src/App.js
+++ public/src/App.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('.js-todo-list-checkbox').forEach(function(checkbox){
   const id = checkbox['dataset'].id;
   checkbox.addEventListener('change', async function(event) {
-  const isChecked = event.target.checked === true ? 1 : 0 ;
+  const isChecked = event.target.checked;
   await fetch(`/update/${id}`, {
       method: 'POST',
       body: new URLSearchParams({
